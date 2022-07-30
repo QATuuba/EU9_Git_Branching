b174b974489ce0653fa6d795098e45f09b9262e8
tugba <drtugbaduran@gmail.com>
2022-07-30 18:23:10 +0100

    added negative testing for tc001

--- src/test_cases/TC001.java
+++ src/test_cases/TC001.java
@@ -4,9 +4,11 @@ public class TC001 {
 
     public static void main(String[] args) {
 
-        //testing something
+        // testing something
 
-        //more testing
+        // more testing
+
+        // negative testing
 
     }
 
